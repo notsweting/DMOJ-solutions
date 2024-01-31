cfeca2e9ea711ba8ff7d33e9f7329e9f6bfd4db5
sweting <50222844+notsweting@users.noreply.github.com>
2024-01-31 14:44:04 -0500

    add CCC sol

---
+++ CCC/'12/ccc12s1.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, cnt=0;
+    cin >> n;
+    for(int i=1; i<n-2; i++){
+        for(int j=i+1; j<n-1; j++){
+            for(int k=j+1; k<n; k++){
+                cnt++;
+            }
+        }
+    }
+
+    cout << (n>=4?cnt:0) << endl;
+}
